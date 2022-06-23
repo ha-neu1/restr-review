7c07304ea560d09ac75e2e57da753ec00dcf65a5
hyedi <104494201+hyedi@users.noreply.github.com>
2022-06-23 09:52:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -144,24 +144,6 @@ def add():
 def register():
     return render_template("register.html")
 
-@app.route("/member", methods=["get"])
-def members_get():
-    id_receive = request.form['id_give']
-    password_receive = request.form['password_give']
-    area_receive = request.form['area_give']
-    email_receive = request.form['email_give']
-
-    doc = {
-        'id': id_receive,
-        'password': password_receive,
-        'area' : area_receive,
-        'email': email_receive
-    }
-
-    db.member.insert_one(doc)
-
-    return jsonify({'msg': '등록 완료!'})
-
 @app.route("/reviews", methods=["get"])
 def reviews_get():
     image_receive = request.form['image_give']
