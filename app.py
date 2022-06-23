1121be384864ea67754b858a9f216b416b5d5cbf
vincent192 <107832349+vincent192@users.noreply.github.com>
2022-06-23 16:54:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,21 @@ import hashlib
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
+
 app = Flask(__name__)
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.bitceih.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.ehtpe4p.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
+
 
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"username": payload["id"]})
+        user_info = db.users.find_one({"id": payload["id"]})
         return render_template('reviewsave.html')
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
@@ -30,23 +32,23 @@ def login():
     msg = request.args.get("msg")
     return render_template('index.html', msg=msg)
 
+
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
-    
-    username_receive = request.form['username_give']
+    id_receive = request.form['id_give']
     password_receive = request.form['password_give']
 
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+    result = db.users.find_one({'id': id_receive, 'password': pw_hash})
 
     if result is not None:
         payload = {
-            'id': username_receive,
+            'id': id_receive,
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
-        return jsonify({'result': 'success', 'token': token})    
+        return jsonify({'result': 'success', 'token': token})
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
@@ -70,12 +72,11 @@ def sign_up():
     email_receive = request.form['email_give']
     area_receive = request.form['area_give']
 
-
     doc = {
-        "id": id_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "email": email_receive,                           # 프로필 이름 기본값은 아이디
-        "area": area_receive                                        # 프로필 사진 파일 이름
+        "id": id_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "email": email_receive,  # 프로필 이름 기본값은 아이디
+        "area": area_receive  # 프로필 사진 파일 이름
 
     }
     db.users.insert_one(doc)
